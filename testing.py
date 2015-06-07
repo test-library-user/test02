71b46eba01365bc3f6fc5beca51b6de6fc610ef3
Jordan Storz <jordanstorz@Jordans-Mac-mini.local>
2015-06-06 19:53:51 -0600

    Added changes to test file here

--- testing.py
+++ testing.py
@@ -1,6 +1,35 @@
-class Fighter(object) :
-	def __init__(self, name, job)
+class Pidgey(object) :
+	
+	def __init__(self, name) :
 		self.name = name
-		self.job = job
-	hitPoints = 100
-	mana = 100
+		
+	level = 7
+	
+	attributes = {
+					'attack' : 11,
+					'defense' : 8,
+					'speed' : 14
+				  }
+	modifiers = {
+					'attack' : 1.15,
+					'defense' : 1.12,
+					'speed' : 1.37
+				}
+				  
+def level_up(pokemon) :
+	pokemon.level += 1
+	print '%s rises to level %i!' % (pokemon.name, pokemon.level)
+	for each in pokemon.attributes :
+		pokemon.attributes[each] += pokemon.modifiers[each] * pokemon.level - pokemon.level
+		print each, pokemon.attributes[each]
+	
+	
+bill = Pidgey('Bill')
+
+print bill.level
+print bill.attributes['attack']
+
+
+	
+level_up(bill)
+level_up(bill)
